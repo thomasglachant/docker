436e101b5de0bfb4cb249470eb526364566ca211
Thomas Glachant <thomas@glachant.fr>
2017-02-10 14:28:22 +0100

    Improve directories

--- build-all-images.sh
+++ build-all-images.sh
@@ -1,35 +1,40 @@
 #!/usr/bin/env bash
 
-# Go to script directory
-basedir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # For all versions : launch build
 for VERSION in `ls ${basedir}`; do
-	# Script directory
-	VERSION_DIR=${basedir}/${VERSION}
+	# Version directory
+	VERSION_DIR=${SCRIPTPATH}/${VERSION}
 
-	# Version directory does not exist
+	# Explore only directories so exit if not directory
 	if [[ ! -d $VERSION_DIR ]]; then
-		printf "Error : Version \"$VERSION\" does not exist\n";
-		exit -1;
+		break;
 	fi
 
-	# Go to dir
-	cd ${VERSION_DIR}
+	for VARIANT in `ls ${VERSION_DIR}`; do
+		# Variant directory
+		VARIANT_DIR=${VERSION_DIR}/${VARIANT}
+		
+		# If the directory does not contain a docker file : exit
+		if [[ ! -d ${VARIANT_DIR} ]] || [[ ! -f ${VARIANT_DIR}/Dockerfile ]]; then
+			break
+		fi
 
-	printf "============================\n==> Start build \"$VERSION\" <==\n============================\n";
+		printf "============================\n==> Start build \"$VERSION-${VARIANT}\" <==\n============================\n";
 
-	# Build image
-	docker build -t thomasglachant/docker-php:php${VERSION} .
+		# Build image
+		docker build -t thomasglachant/docker-php:php${VERSION} $VARIANT_DIR
 
-	# get build return code 
-	BUILD_SUCCESS=$?
+		# get build return code 
+		BUILD_SUCCESS=$?
 
-	printf "============================\n==> End build \"$VERSION\" <==\n============================\n\n";
+		printf "============================\n==> End build \"$VERSION-${VARIANT}\" <==\n============================\n\n";
 
-	# if build fail : exit
-	if [[ ${BUILD_SUCCESS} -ne 0 ]] ; then
-		printf "Error : Unable to build version \"${VERSION}\"\n";
-		exit -1;
-	fi
+		# if build fail : exit
+		if [[ ${BUILD_SUCCESS} -ne 0 ]] ; then
+			printf "Error : Unable to build version \"${VERSION}-${VARIANT}\"\n";
+			exit -1;
+		fi
+	done
 done
