f03d09db9154dadbf8e31cd41db273ba75ef13bb
Thomas Glachant <thomas@glachant.fr>
2017-02-10 14:08:32 +0100

    Improve build image and travis

--- build-all-images.sh
+++ build-all-images.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
 # Go to script directory
-dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-cd $dir
+basedir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 # For all versions : launch build
-for version in 5.6-cli 5.6-fpm 7-cli 7-fpm; do
-	# launch build
-	bash $dir/build-image.sh $version
+for VERSION in `ls ${basedir}`; do
+	# Script directory
+	VERSION_DIR=${basedir}/${VERSION}
+
+	# Version directory does not exist
+	if [[ ! -d $VERSION_DIR ]]; then
+		printf "Error : Version \"$VERSION\" does not exist\n";
+		exit -1;
+	fi
+
+	# Go to dir
+	cd ${VERSION_DIR}
+
+	printf "============================\n==> Start build \"$VERSION\" <==\n============================\n";
+
+	# Build image
+	docker build -t thomasglachant/docker-php:php${VERSION} .
+
+	# get build return code 
+	BUILD_SUCCESS=$?
+
+	printf "============================\n==> End build \"$VERSION\" <==\n============================\n\n";
 
 	# if build fail : exit
-	if [[ $BUILD_SUCCESS -ne 0 ]] ; then
-		printf "Error : Unable to build version \"$version\"\n";
+	if [[ ${BUILD_SUCCESS} -ne 0 ]] ; then
+		printf "Error : Unable to build version \"${VERSION}\"\n";
 		exit -1;
 	fi
 done
