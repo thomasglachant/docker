8e653769030f3c27de9baab3a6a02cf4ac944682
Thomas Glachant <thomas@glachant.fr>
2017-02-17 09:08:45 +0100

    Remove cache clear : just give rights when start cli

--- php/7.1/cli/docker-php-entrypoint.sh
+++ php/7.1/cli/docker-php-entrypoint.sh
@@ -5,8 +5,8 @@ set -xe
 /bin/bash docker-php-configure
 
 # Give rights for cache & logs (sf1 & sf2)
-if [ -d cache ]; then rm -rf cache/*; chmod -R 777 cache; fi
-if [ -d app/cache ]; then rm -rf app/cache/*; chmod -R 777 app/cache; fi
+if [ -d cache ]; then chmod -R 777 cache; fi
+if [ -d app/cache ]; then chmod -R 777 app/cache; fi
 if [ -d log ]; then chmod -R 777 log; fi
 if [ -d app/logs ]; then chmod -R 777 app/logs; fi
 
