ca1a439de94f5a99f459213dbc0988893f1121de
Thomas Glachant <thomas@glachant.fr>
2017-02-17 11:16:28 +0100

    Improve cli entrypoint and cmd configuration

--- php/7.1/cli/docker-php-entrypoint.sh
+++ php/7.1/cli/docker-php-entrypoint.sh
@@ -10,5 +10,13 @@ if [ -d app/cache ]; then chmod -R 777 app/cache; fi
 if [ -d log ]; then chmod -R 777 log; fi
 if [ -d app/logs ]; then chmod -R 777 app/logs; fi
 
-# Infinite loop
-/bin/bash -c "trap : TERM INT; sleep infinity & wait"
+# Launch
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php "$@"
+    exec "$@"
+else
+    # Infinite loop
+    /bin/bash -c "trap : TERM INT; sleep infinity & wait"
+fi
+
