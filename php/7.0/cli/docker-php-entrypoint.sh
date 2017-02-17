b22cad0f632067845b0747f1036a8b7f8962f53c
Thomas Glachant <thomas@glachant.fr>
2017-02-17 09:07:19 +0100

    Add cache clear and give rights on start cli

--- php/7.0/cli/docker-php-entrypoint.sh
+++ php/7.0/cli/docker-php-entrypoint.sh
@@ -4,5 +4,11 @@ set -xe
 # Configure
 /bin/bash docker-php-configure
 
+# Give rights for cache & logs (sf1 & sf2)
+if [ -d cache ]; then rm -rf cache/*; chmod -R 777 cache; fi
+if [ -d app/cache ]; then rm -rf app/cache/*; chmod -R 777 app/cache; fi
+if [ -d log ]; then chmod -R 777 log; fi
+if [ -d app/logs ]; then chmod -R 777 app/logs; fi
+
 # Infinite loop
 /bin/bash -c "trap : TERM INT; sleep infinity & wait"
