cab7015c6f253f3f035905685281b8363ca24035
Thomas Glachant <thomas@glachant.fr>
2017-02-22 13:17:54 +0100

    Add Sf3 support fro entrypoint

--- php/5.6/cli/docker-php-entrypoint.sh
+++ php/5.6/cli/docker-php-entrypoint.sh
@@ -4,11 +4,13 @@ set -xe
 # Configure
 /bin/bash docker-php-configure
 
-# Give rights for cache & logs (sf1 & sf2)
+# Give rights for cache & logs (sf1 & sf2 & sf3)
 if [ -d cache ]; then chmod -R 777 cache; fi
 if [ -d app/cache ]; then chmod -R 777 app/cache; fi
+if [ -d var/cache ]; then chmod -R 777 var/cache; fi
 if [ -d log ]; then chmod -R 777 log; fi
 if [ -d app/logs ]; then chmod -R 777 app/logs; fi
+if [ -d var/logs ]; then chmod -R 777 var/logs; fi
 
 # Launch
 # first arg is `-f` or `--some-option`
