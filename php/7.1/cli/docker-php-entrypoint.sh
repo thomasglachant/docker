828d7a04cd3f0b3ce746de907a8adf8e1d424ae8
Thomas Glachant <thomas@glachant.fr>
2017-03-03 17:42:42 +0100

    Try new rights method

--- php/7.1/cli/docker-php-entrypoint.sh
+++ php/7.1/cli/docker-php-entrypoint.sh
@@ -5,7 +5,7 @@ set -xe
 /bin/bash docker-php-configure
 
 # Give rights for cache & logs (sf1 & sf2 & sf3)
-chown -R www-data var app/cache app/logs cache log || true
+chown -R www-data:www-data var var/cache var/logs var/sessions app/cache app/logs cache log || true
 
 # Launch
 if [ "$#" -ge 1 ]; then
